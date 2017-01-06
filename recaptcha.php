51fb87936147fb6ad4f6852e40e967ecd1b50442
shankarmehta <shankarmehta785@gmail.com>
2017-01-06 11:05:16 +0530

    Update recaptcha.php

--- recaptcha.php
+++ recaptcha.php
@@ -1,18 +1,15 @@
 <?php 
-
 if (isset($_POST['submit'])) {
-    $secret = '6LfE5hQTAAAAAIU5P_IQjg8JIZ_x4Rd4dSzysgSC';
+    $secret = '6LcZzRAUAAAAAH6UzK7FwV3K7RtlzIf_tKB4J-bX';
     $response = $_POST['g-recaptcha-response'];
     $remoteip = $_SERVER['REMOTE_ADDR'];
     
-    $url = file_get_contents("https://www.google.com/recaptcha/api/siteverify?secret=$secret&response=$response&remoteip=$remoteip");
+    $url = file_get_contents("https://www.google.com/recaptcha/api/siteverify?secret=6LcZzRAUAAAAAH6UzK7FwV3K7RtlzIf_tKB4J-bX&response=$response&remoteip=$remoteip");
     $result = json_decode($url, TRUE);
-
     if ($result['success'] == 1) {
         echo $_POST['myreq'];
     }
 }
-
 ?>
 <!doctype html>
 <html lang="en">
@@ -25,7 +22,7 @@ if (isset($_POST['submit'])) {
     <br><br>
     <form action="" method="post">
         <input type="text" name="myreq"><br>
-        <div class="g-recaptcha" data-sitekey="6LfE5hQTAAAAAH2Aj5lMPSA4yUQzsEDGxoJbV4Ib"></div>
+       <div class="g-recaptcha" data-sitekey="6LcZzRAUAAAAAHIsfLJG2tp48jYwBnVa8X2AAE4a"></div>
         <input type="submit" name="submit" value="Send Request!">
     </form>
 </body>
